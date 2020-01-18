8c6e97dfd4eb25103cf3fd91804679535026fac8
Mitchell Hollander <MitchellSHollander@gmail.com>
2020-01-18 02:00:19 -0600

    added more content to movie details page

--- app/controllers/movie_details_controller.rb
+++ app/controllers/movie_details_controller.rb
@@ -4,12 +4,20 @@ class MovieDetailsController < ApplicationController
   def show
     details = Tmdb::Movie.detail(params[:id])
     details.cast = Tmdb::Movie.cast(params[:id])
+    details.reviews = Tmdb::Movie.reviews(params[:id]).results
     release = details.release_date
     if release.nil? || release == ''
       details.release_date = 'TBD'
     else
       details.release_date = "#{Date::MONTHNAMES[release.slice(5..6).to_i]} #{release.slice(8..9)}, #{release.slice(0..3)}"
     end
+    details.runtime = time_conversion(details.runtime)
     @film = details
+  end
+
+  def time_conversion(minutes)
+    hours = minutes / 60
+    rest = minutes % 60
+    return "#{hours}h #{rest}m"
   end
 end
